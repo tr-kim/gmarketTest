f60f3b162dd3a25cc87081bd58538e44535f02ac
hyunjiq <edalnim12@gmail.com>
2025-06-16 17:54:38 +0900

    topmenu js수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,20 +1,31 @@
 window.addEventListener('load', function() {
     //메뉴 클릭
-    if(document.querySelector('header')){
+    if (document.querySelector('header')) {
         const lis = document.querySelectorAll('header li');
-        lis.forEach(li =>{
-            li.addEventListener('click', function(){            
-                lis.forEach(item => {
-                    item.classList.remove('active');                
-                })
-                li.classList.add('active');
-            })
-        })
         const sendmenu = document.querySelector('.gnb .sendmenu');
-        sendmenu.addEventListener('click', function() {
-            sendmenu.querySelector('ul').classList.toggle('d-flex');
-        })   
-    }    
+        const sendmenuUl = sendmenu.querySelector('ul');
+
+        lis.forEach(li => {
+            li.addEventListener('click', function () {
+                lis.forEach(item => {
+                    item.classList.remove('active');
+                });
+                li.classList.add('active');
+            });
+        });
+
+        sendmenu.addEventListener('click', function (e) {
+            e.stopPropagation(); // 클릭 이벤트 전파 막기
+            sendmenuUl.classList.toggle('d-flex');
+        });
+
+        // sendmenu 외의 영역 클릭 시 메뉴 닫기
+        document.addEventListener('click', function (e) {
+            if (!sendmenu.contains(e.target)) {
+                sendmenuUl.classList.remove('d-flex');
+            }
+        });
+    }
     if(document.querySelector('aside')){
         const lis = document.querySelectorAll('aside .gnb li');
         lis.forEach(li =>{
