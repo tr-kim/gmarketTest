282e34f4c54d69eef0df41946c8acde0bc643388
hyunjiq <edalnim12@gmail.com>
2025-06-18 13:47:34 +0900

    커밋

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
-    //메뉴 클릭
+    
     if (document.querySelector('header')) {
+        //메뉴 클릭
         const lis = document.querySelectorAll('header li');
         const sendmenu = document.querySelector('.gnb .sendmenu');
         const sendmenuUl = sendmenu.querySelector('ul');
@@ -25,8 +26,29 @@ window.addEventListener('load', function() {
                 sendmenuUl.classList.remove('d-flex');
             }
         });
+
+        //사용자 클릭시 비밀번호 변경 버튼
+        const user_profile = document.querySelector('.user_profile');
+        const psw_chg_btn = document.querySelector('.psw_chg_btn');
+        const passwordChg = document.querySelector('.passwordChg');
+
+        user_profile.addEventListener('click',function(e){
+            e.stopPropagation();
+            psw_chg_btn.classList.toggle('d-block');
+        })
+        document.addEventListener('click', function (e) {
+            if (!user_profile.contains(e.target)) {
+                psw_chg_btn.classList.remove('d-block');
+            }
+        });
+        psw_chg_btn.addEventListener('click', function(){
+            console.log(passwordChg)
+            passwordChg.classList.add('d-block');
+        });
+
     }
     if(document.querySelector('aside')){
+        //메뉴 클릭
         const lis = document.querySelectorAll('aside .gnb li');
         lis.forEach(li =>{
             li.addEventListener('click', function(){            
@@ -42,5 +64,39 @@ window.addEventListener('load', function() {
         document.querySelector('.fixed_btn').addEventListener('click', function() {
             document.getElementById('gm_login').classList.toggle('light_mode');
         })
+    }
+
+    //비밀번호 변경
+    const passwordChg = document.querySelector('.passwordChg');
+    if(passwordChg){    
+        const pw1 = document.getElementById('new-psw');
+        const pw2 = document.getElementById('new-psw2');
+        const message = document.getElementById('psw-ck');
+        const closebtn = document.querySelector('.close_btn');
+        const inputs = document.querySelectorAll('.passwordChg input');
+
+        function checkPasswordMatch() {
+            if (pw1.value && pw2.value) {
+                if (pw1.value === pw2.value) {
+                    message.textContent = '비밀번호가 일치합니다.';
+                    message.style.color = '#333';
+                } else {
+                    message.textContent = '비밀번호가 일치하지 않습니다.';
+                    message.style.color = '#ff6767';
+                }
+            } else {
+                message.textContent = '';
+            }
+        }
+
+        pw1.addEventListener('input', checkPasswordMatch);
+        pw2.addEventListener('input', checkPasswordMatch);
+
+        closebtn.addEventListener('click',function(){
+            inputs.forEach(input =>{
+                input.value= "";
+            });
+            passwordChg.classList.remove('d-block');
+        });
     }
 })
