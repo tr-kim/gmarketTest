82a97ab427bfb22bff25e48dd6ae3366883601bd
tr-kim <tr.kim@dinnovan.com>
2025-06-20 10:10:21 +0900

    dxDataGrid 샘플

---
+++ src/main/resources/static/js/view/hist.js
@@ -0,0 +1,89 @@
+$(function () {
+	const startDate = new Date();
+	const endDate = new Date();
+	endDate.setDate(endDate.getDate() + 7);
+	
+	//조회 그리드
+	$("#histGrid").dxDataGrid({
+		dataSource: [
+			{ idx: 1, msg_key: "0000001", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 2, msg_key: "0000002", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 3, msg_key: "0000003", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 4, msg_key: "0000004", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 5, msg_key: "0000005", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 6, msg_key: "0000006", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 7, msg_key: "0000007", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 8, msg_key: "0000008", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 9, msg_key: "0000009", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 10, msg_key: "0000010", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 11, msg_key: "0000011", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." },
+			{ idx: 12, msg_key: "0000012", msg_type: "SMS", callback_no: "010-1234-5678", caller_no: "010-8765-4321", gubun1: "LG", gubun2: "LG", status1: "완료", status2: "기타오류(999)", msg_body: "SMS 발송 테스트입니다." }
+		],
+		headerFilter: {
+			visible: true
+		},
+		searchPanel: {
+			visible: true,
+			width: 250
+		},
+		paging: {
+			pageSize: 10
+		},
+		pager: {
+			showInfo: true,
+			showNavigationButtons: true,
+			showPageSizeSelector: true,
+			allowedPageSizes: [10, 20, 30]
+		},
+		columnAutoWidth: true,
+		allowColumnResizing: true,
+		columnResizingMode: 'widget',
+		columns: [
+			{ dataField: "idx", caption: "NO" },
+			{ dataField: "msg_key", caption: "MSG_KEY" },
+			{ dataField: "msg_type", caption: "메시지 종류" },
+			{ dataField: "callback_no", caption: "발신 번호" },
+			{ dataField: "caller_no", caption: "수신 번호" },
+			{ dataField: "gubun1", caption: "이통사" },
+			{ dataField: "gubun2", caption: "발송사" },
+			{ dataField: "status1", caption: "전송상태" },
+			{ dataField: "status2", caption: "전송결과" },
+			{ dataField: "msg_body", caption: "내용" }
+		],
+		toolbar: {
+			items: [
+				{
+					location: "after",
+					widget: "dxButton",
+					options: {
+						text: "엑셀 다운로드",
+						type: "default",
+						width: 130,
+						onClick(e) {
+							const grid = $("#smsResultGrid").dxDataGrid("instance");
+							exportGridToExcel(grid);
+						},
+					},
+				},
+				"searchPanel"
+			]
+		},
+	}).dxDataGrid("instance");
+});
+
+//엑셀 다운로드
+function exportGridToExcel(gridInstance){
+	const workbook = new ExcelJS.Workbook();
+	const worksheet = workbook.addWorksheet('문자발송결과');
+	
+	DevExpress.excelExporter.exportDataGrid({
+		component: gridInstance,
+		worksheet: worksheet,
+		autoFilterEnabled: true,
+	}).then(() => {
+		workbook.xlsx.writeBuffer().then((buffer) => {
+			saveAs(new Blob([buffer], { type: 'application/octet-stream' }), '문자발송결과.xlsx');
+		});
+	});
+}
+
