d83cd6bc342ec9de14bcafc2a99de5a0b28ad10f
hyunjiq <edalnim12@gmail.com>
2025-06-25 14:00:22 +0900

    개별발송 수정

--- src/main/resources/static/js/view/send.js
+++ src/main/resources/static/js/view/send.js
@@ -54,144 +54,6 @@ $(function () {
             });
         });
     });
-
-	//직접입력 그리드
-	$("#directGrid").dxDataGrid({
-		dataSource: [
-			{ idx: 1, name: "홍길동", callback_no: "010-0000-0001" },
-            { idx: 2, name: "홍길서", callback_no: "010-0000-0002" },
-            { idx: 3, name: "홍길남", callback_no: "010-0000-0003" },
-            { idx: 4, name: "홍길북", callback_no: "010-0000-0004" },
-            { idx: 5, name: "홍길지", callback_no: "010-0000-0005" },
-            { idx: 6, name: "홍길마", callback_no: "010-0000-0006" },
-            { idx: 7, name: "홍길켓", callback_no: "010-0000-0007" },
-            { idx: 8, name: "홍길디", callback_no: "010-0000-0008" },
-            { idx: 9, name: "홍길노", callback_no: "010-0000-0009" },
-            { idx: 10,name: "홍길밴", callback_no: "010-0000-0010" },
-		],
-		keyExpr: "idx",
-		headerFilter: {
-			visible: true
-		},
-		paging: {
-			pageSize: 50
-		},
-		pager: {
-			visible: true,
-			showNavigationButtons: true,
-			showPageSizeSelector: true,
-			allowedPageSizes: [50, 100, 200]
-		},
-		columnAutoWidth: true,
-		allowColumnResizing: true,
-		columnResizingMode: 'widget',
-		columns: [
-			{ dataField: "name", caption: "이름" },
-			{ dataField: "callback_no", caption: "전화번호" },						
-            { 
-				dataField: "", 
-				caption: "삭제" , 
-				type: 'buttons', 
-				width: 60,			
-				buttons: [{
-					icon: 'trash',
-					onClick(e) {
-						alert('test');
-					},
-                    cssClass:'text-body-tertiary',
-				}],
-			},			
-		],		
-		onContentReady: function(e) {
-			const totalCount = e.component.totalCount();
-			$("#direct_wrap .count").text(`총 ${totalCount}건`);
-		}		
-	}).dxDataGrid("instance");
-
-    //주소록 그리드
-    $("#addressGrid").dxDataGrid({
-		dataSource: [
-			{ idx: 1, name: "홍길동", callback_no: "010-0000-0001" },
-            { idx: 2, name: "홍길서", callback_no: "010-0000-0002" },
-            { idx: 3, name: "홍길남", callback_no: "010-0000-0003" },
-            { idx: 4, name: "홍길북", callback_no: "010-0000-0004" },
-            { idx: 5, name: "홍길지", callback_no: "010-0000-0005" },
-            { idx: 6, name: "홍길마", callback_no: "010-0000-0006" },
-            { idx: 7, name: "홍길켓", callback_no: "010-0000-0007" },
-            { idx: 8, name: "홍길디", callback_no: "010-0000-0008" },
-            { idx: 9, name: "홍길노", callback_no: "010-0000-0009" },
-            { idx: 10,name: "홍길밴", callback_no: "010-0000-0010" },
-		],
-		keyExpr: "idx",
-		headerFilter: {
-			visible: true
-		},
-		paging: {
-			pageSize: 50
-		},
-		pager: {
-			visible: true,
-			showNavigationButtons: true,
-			showPageSizeSelector: true,
-			allowedPageSizes: [50, 100, 200]
-		},
-		columnAutoWidth: true,
-		allowColumnResizing: true,
-		columnResizingMode: 'widget',
-		columns: [
-			{ dataField: "name", caption: "이름" },
-			{ dataField: "callback_no", caption: "전화번호" },									
-		],		
-		onContentReady: function(e) {
-			const totalCount = e.component.totalCount();
-			$("#address_wrap .count").text(`총 ${totalCount}건`);
-		}		
-	}).dxDataGrid("instance");
-
-    //엑셀등록 그리드
-    $("#excelGrid").dxDataGrid({
-		dataSource: [
-			{ idx: 1, name: "홍길동", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0001" },
-            { idx: 2, name: "홍길서", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0002" },
-            { idx: 3, name: "홍길남", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0003" },
-            { idx: 4, name: "홍길북", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0004" },
-            { idx: 5, name: "홍길지", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0005" },
-            { idx: 6, name: "홍길마", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0006" },
-            { idx: 7, name: "홍길켓", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0007" },
-            { idx: 8, name: "홍길디", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0008" },
-            { idx: 9, name: "홍길노", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0009" },
-            { idx: 10,name: "홍길밴", tag1: "변수1", tag2: "변수2", tag3: "변수3", tag4: "변수4",  tag5: "변수5",  callback_no: "010-0000-0010" },
-		],
-		keyExpr: "idx",
-		headerFilter: {
-			visible: true
-		},
-		paging: {
-			pageSize: 50
-		},
-		pager: {
-			visible: true,
-			showNavigationButtons: true,
-			showPageSizeSelector: true,
-			allowedPageSizes: [50, 100, 200]
-		},
-		columnAutoWidth: true,
-		allowColumnResizing: true,
-		columnResizingMode: 'widget',
-		columns: [
-			{ dataField: "name", caption: "이름" },
-			{ dataField: "callback_no", caption: "전화번호" },	
-            { dataField: "tag1", caption: "변수1" },
-            { dataField: "tag2", caption: "변수2" },
-            { dataField: "tag3", caption: "변수3" },
-            { dataField: "tag4", caption: "변수4" },
-            { dataField: "tag5", caption: "변수5" },
-		],		
-		onContentReady: function(e) {
-			const totalCount = e.component.totalCount();
-			$("#excel_wrap .count").text(`총 ${totalCount}건`);
-		}		
-	}).dxDataGrid("instance");
      
     //이미지 등록
     $('#file-uploader').dxFileUploader({
@@ -220,18 +82,15 @@ $(function () {
         onDropZoneLeave(e) {
             if (e.dropZoneElement.id === 'dropzone-external') { toggleDropZoneActive(e.dropZoneElement, false); }
         },
-        onFilesChanged(e) {
-            const maxFiles = 3;
-            const files = e.component.option('value');
-
-            if (files.length > maxFiles) {
-                // 초과한 파일 제거
-                const trimmedFiles = files.slice(0, maxFiles);
-                e.component.option('value', trimmedFiles);
-                alert('이미지는 최대 3개까지만 업로드할 수 있습니다.');
-            }
-        },
+       
         onUploaded(e) {
+            const uploadedCount = document.querySelectorAll('#dropzone-image-list .col-4').length;
+
+            if (uploadedCount >= 3) {
+                alert('이미지는 최대 3개까지만 업로드할 수 있습니다.');
+                return;
+            }
+
             const { file } = e;
             const fileReader = new FileReader();
 
@@ -239,19 +98,17 @@ $(function () {
                 toggleDropZoneActive(document.getElementById('dropzone-external'), false);
 
                 const colDiv = document.createElement('div');
-                colDiv.className = 'col-4 position-relative'; // 위치 relative로
+                colDiv.className = 'col-4 position-relative';
 
                 const img = document.createElement('img');
                 img.src = fileReader.result;
                 img.classList.add('img-fluid', 'rounded');
 
-                // 삭제 버튼
                 const deleteBtn = document.createElement('button');
                 deleteBtn.innerHTML = '&times;';
                 deleteBtn.className = 'btn btn-sm btn-dark position-absolute top-0 end-0 m-1';
                 deleteBtn.style.zIndex = '10';
 
-                // 삭제 이벤트
                 deleteBtn.addEventListener('click', () => {
                     colDiv.remove();
                 });
@@ -295,11 +152,182 @@ $(function () {
         dropZoneImage.hidden = !visible;
     }
 
-    document.getElementById('dropzone-image').onload = function () { toggleImageVisible(true); };
+    document.getElementById('dropzone-image-list').onload = function () { toggleImageVisible(true); };
 
-    //메세지 플레이스홀더
-   
+    //예약 발송 캘린더
+    const zoomLevels = ['month', 'year', 'decade', 'century'];
+    const weekDays = [
+        { id: 0, text: 'Sunday' },
+        { id: 1, text: 'Monday' },
+        { id: 2, text: 'Tuesday' },
+        { id: 3, text: 'Wednesday' },
+        { id: 4, text: 'Thursday' },
+        { id: 5, text: 'Friday' },
+        { id: 6, text: 'Saturday' },
+    ];
+    const weekNumberRules = ['auto', 'firstDay', 'firstFourDays', 'fullWeek'];
 
+    let reserveDate = "";
+
+    const calendar = $('#calendar').dxCalendar({
+        value: new Date(),
+        disabled: false,
+        firstDayOfWeek: 0,
+        showWeekNumbers: false,
+        weekNumberRule: 'auto',
+        zoomLevel: zoomLevels[0],
+        onValueChanged(data) {            
+            const date = data.value;
+            if (date instanceof Date && !isNaN(date)) {
+                const yy = String(date.getFullYear()).slice(2);
+                const mm = String(date.getMonth() + 1).padStart(2, '0');
+                const dd = String(date.getDate()).padStart(2, '0');
+
+                document.querySelector('.reserveSend .date').textContent=`${yy}-${mm}-${dd}`
+                console.log(`${yy}${mm}${dd}`);
+
+                reserveDate = `${yy}-${mm}-${dd}`
+            }
+        },
+        onOptionChanged(data) {
+        if (data.name === 'zoomLevel') {
+            zoomLevel.option('value', data.value);
+        }
+        },
+        
+    }).dxCalendar('instance');
+
+    const zoomLevel = $('#zoom-level').dxSelectBox({
+        dataSource: zoomLevels,
+        value: zoomLevels[0],
+        inputAttr: { 'aria-label': 'Zoom Level' },
+        onValueChanged(data) {
+        calendar.option('zoomLevel', data.value);
+        },
+    }).dxSelectBox('instance');
+
+    const selectedDate = $('#selected-date').dxDateBox({
+        value: new Date(),
+        inputAttr: { 'aria-label': 'Date' },
+        onValueChanged(data) {
+        calendar.option('value', data.value);
+        },
+    }).dxDateBox('instance');
+
+    $('#custom-cell').dxCheckBox({
+        text: 'Use custom cell template',
+        value: false,
+        onValueChanged(data) {
+        calendar.option('cellTemplate', data.value ? getCellTemplate : 'cell');
+        },
+    });
+
+    $('#disabled').dxCheckBox({
+        text: 'Disable the calendar',
+        onValueChanged(data) {
+        calendar.option('disabled', data.value);
+        },
+    });
+
+    $('#week-numbers').dxCheckBox({
+        text: 'Show week numbers',
+        onValueChanged(data) {
+        calendar.option('showWeekNumbers', data.value);
+        },
+    });
+
+    $('#first-day-of-week').dxSelectBox({
+        dataSource: weekDays,
+        value: 0,
+        valueExpr: 'id',
+        inputAttr: { 'aria-label': 'First Day of Week' },
+        displayExpr: 'text',
+        onValueChanged(data) {
+        calendar.option('firstDayOfWeek', data.value);
+        },
+    });
+
+    $('#week-number-rule').dxSelectBox({
+        dataSource: weekNumberRules,
+        value: weekNumberRules[0],
+        inputAttr: { 'aria-label': 'Week Number Rule' },
+        onValueChanged(data) {
+        calendar.option('weekNumberRule', data.value);
+        },
+    });
+
+    const holidays = [[1, 0], [4, 6], [25, 11]];
+
+    function isWeekend(d) {
+        const day = d.getDay();
+
+        return day === 0 || day === 6;
+    }
+
+    function getCellTemplate(data) {
+        let cssClass = '';
+
+        if (data.view === 'month') {
+        if (!data.date) {
+            cssClass = 'week-number';
+        } else {
+            if (isWeekend(data.date)) { cssClass = 'weekend'; }
+
+            $.each(holidays, (_, item) => {
+            if (data.date.getDate() === item[0] && data.date.getMonth() === item[1]) {
+                cssClass = 'holiday';
+                return false;
+            }
+            return true;
+            });
+        }
+        }
+
+        return `<span class='${cssClass}'>${data.text}</span>`;
+    }
+
+    //예약 발송 모달 열기,닫기
+    const reserveModal = document.querySelector('.reserveSend');
+    const selects = document.querySelectorAll('select');
+    const reserveHour = document.getElementById('hour').value;
+    const reserveMinute = document.getElementById('minute').value;
+
+    document.getElementById('send_time1').addEventListener('change',function(){
+        if (this.checked) {
+            document.getElementById('reserveDate').textContent = "";
+        }
+    });
+
+    document.getElementById('send_time2').addEventListener('click', function () {
+        if (this.checked) {
+            reserveModal.classList.add("d-block");
+        }
+    });
+
+    document.querySelector('.reserveSend .close_btn').addEventListener('click', function(){
+        reserveModal.classList.remove("d-block");
+
+        document.getElementById('send_time1').checked = true;
+
+        document.querySelector('.date').textContent = '날짜를 선택해 주세요.';
+        selects.forEach(select => {
+            select.value = "00";
+        })
+    })
+
+    document.querySelector('.reserveSend .modal-ft button').addEventListener('click', function(){
+        if(reserveDate == ""){
+            alert('날짜를 선택해 주세요.')
+            return;
+        }
+        document.getElementById('reserveDate').textContent = `예약시간: ${reserveDate} ${reserveHour}:${reserveMinute}`
+
+        reserveModal.classList.remove("d-block");
+        document.querySelector('.date').textContent = '날짜를 선택해 주세요.';
+        selects.forEach(select => {
+            select.value = "00";
+        })
+    })
 });
     
 
