bc814eec8dc66b4c1cbb538faa0dce92909549a6
tr-kim <tr.kim@dinnovan.com>
2025-09-18 09:17:54 +0900

    모니터링 샘플 진행중

--- src/main/java/com/web/gmarket/real/controller/RealSendHistController.java
+++ src/main/java/com/web/gmarket/real/controller/RealSendHistController.java
@@ -29,18 +29,39 @@ public class RealSendHistController {
 		return "view/real_send_hist";
 	}
 	
+	@GetMapping("/real/left2")
+	public String left2(Model model) {
+		
+		model.addAttribute(ConstantsUtils.LAYOUT, "/layouts/left_layout");
+		model.addAttribute(ConstantsUtils.ACTIVE, ConstantsUtils.REAL);
+		
+		return "view/real_send_hist_old2";
+	}
+	
+	@GetMapping("/real/left3")
+	public String left3(Model model) {
+		
+		model.addAttribute(ConstantsUtils.LAYOUT, "/layouts/left_layout");
+		model.addAttribute(ConstantsUtils.ACTIVE, ConstantsUtils.REAL);
+		
+		return "view/real_send_hist_old3";
+	}
+	
+	// 지마켓 상세
 	@GetMapping("/real/detail/gmarket")
 	public String gmarketDetail() {
 		
 		return "detailGmarket";
 	}
 	
+	// 옥션 상세
 	@GetMapping("/real/detail/auction")
 	public String auctionDetail() {
 		
 		return "detailAuction";
 	}
 	
+	// 스마일캐시 상세
 	@GetMapping("/real/detail/smilecash")
 	public String smilecashDetail() {
 		
