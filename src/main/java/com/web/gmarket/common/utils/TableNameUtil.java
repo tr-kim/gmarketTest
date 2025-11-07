a31cb988e3cac56a04e863ad10ea774f03ca8eac
tr-kim <tr.kim@dinnovan.com>
2025-11-07 17:12:15 +0900

    기존 소스 GMARKET_HIST_DB: gmarket_hist

--- src/main/java/com/web/gmarket/common/utils/TableNameUtil.java
+++ src/main/java/com/web/gmarket/common/utils/TableNameUtil.java
@@ -44,7 +44,7 @@ public class TableNameUtil {
 	public static boolean tableExists(Integer companyCode, String tableName, JdbcTemplate jdbcTemplate) {
 		// String sql = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ?";
 		
-		String schema = (companyCode == 0) ? "" : "msdbgm."; // 지마켓 분기(기존 GMARKET_HIST_DB)
+		String schema = (companyCode == 0) ? "" : "msdbgm."; // 지마켓 스키마 구분(기존 GMARKET_HIST_DB: gmarket_hist)
 		String sql = String.format("SELECT COUNT(*) FROM %sINFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ?", schema);
 		
 		Integer count = jdbcTemplate.queryForObject(sql, Integer.class, tableName);
