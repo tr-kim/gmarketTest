5800dc98a4da7a90ea9af3accfc344ef1dc2f15d
tr-kim <tr.kim@dinnovan.com>
2025-11-10 10:52:51 +0900

    TableNameUtil.getMonthTableNames 호출 수정

--- src/main/java/com/web/gmarket/bulk/hist/service/impl/BulkHistServiceImpl.java
+++ src/main/java/com/web/gmarket/bulk/hist/service/impl/BulkHistServiceImpl.java
@@ -29,7 +29,7 @@ public class BulkHistServiceImpl implements BulkHistService {
 		this.dynamicDataSourceService = dynamicDataSourceService;
 	}
 	
-	//타입별 테이블명 치환
+	// 타입별 테이블명 치환
 	private static final Map<String, String> SVC_TYPE_TABLE_MAP = new HashMap<>();
 	static {
 		SVC_TYPE_TABLE_MAP.put(ConstantsUtils.EXCEL_SMS, ConstantsUtils.SMSCLI_TBL_EVENT);
@@ -46,40 +46,46 @@ public class BulkHistServiceImpl implements BulkHistService {
 		SVC_TYPE_TABLE_MAP.put(ConstantsUtils.SINGLE_MMS, ConstantsUtils.MMSCLI_TBL_EVENT);
 	}
 	
-	/*@Override
-	public List<BulkHistDto> getBulkHistList(BulkHistDto bulkHistDto) {
-		return bulkHistMapper.selectBulkHistList(bulkHistDto);
-	}*/
-	
-	//목록 조회 후 상태 카운트 병합
+	// 목록 조회 후 상태 카운트 병합
 	@Override
 	public List<BulkHistDto> getBulkHistList(BulkHistDto bulkHistDto) {
 		Integer companyCode = bulkHistDto.getCompanyCode();
 		String dbName = DBUtils.getDBName(companyCode);
 		
-		//목록 조회
+		// 목록 조회
 		List<BulkHistDto> list = getMapper(dbName).selectBulkHistList(bulkHistDto);
 		
 		String startMonth = bulkHistDto.getStartDate();
 		String endMonth = bulkHistDto.getEndDate();
 		
-		//각 row 상태 카운트 조회 및 병합
+		// 각 row 상태 카운트 조회 및 병합
 		for (BulkHistDto row : list) {
 			String svcType = row.getSvcType();
-			String tableName = SVC_TYPE_TABLE_MAP.getOrDefault(svcType, ConstantsUtils.SMSCLI_TBL_EVENT); // 없을 경우 기본값
+			String tableName = SVC_TYPE_TABLE_MAP.get(svcType);
 			
-			//대량발송 메시지 키
+			if (tableName == null) {
+				throw new IllegalArgumentException("지원하지 않는 타입: " + svcType);
+			}
+			
+			// 대량발송 메시지 키
 			bulkHistDto.setBulkMsgKey(row.getBulkMsgKey());
 			
-			//월별 리스트 생성
-			List<String> tableList = TableNameUtil.getMonthTableNames(companyCode, startMonth, endMonth, tableName, jdbcTemplateProvider.getJdbcTemplate(dbName));
+			// 월별 테이블 존재 여부 확인 및 목록 생성
+			List<String> tableList = TableNameUtil.getMonthTableNames(
+					companyCode,
+					startMonth,
+					endMonth,
+					tableName,
+					jdbcTemplateProvider.getJdbcTemplate(dbName)
+			);
 			
+			// 결과 DTO에 세팅
 			bulkHistDto.setMonthTables(tableList);
 			
-			//상태 카운트 조회
+			// 상태 카운트 조회
 			Map<String, Integer> result = getMapper(dbName).selectBulkHistStatusCount(bulkHistDto);
 			
-			//조회된 카운트 병합
+			// 조회된 카운트 병합
 			row.setCntStanby(result.getOrDefault(ConstantsUtils.CNT_STANBY, 0)); 			// 전송대기
 			row.setCntTran(result.getOrDefault(ConstantsUtils.CNT_TRAN, 0)); 				// 전송중
 			row.setCntSucc(result.getOrDefault(ConstantsUtils.CNT_SUCC, 0)); 				// 성공
@@ -108,14 +114,26 @@ public class BulkHistServiceImpl implements BulkHistService {
 		Integer companyCode = bulkHistDto.getCompanyCode();
 		String dbName = DBUtils.getDBName(companyCode);
 		
-		// 월별 리스트 생성
+		// 월별 리스트 목록 계산
 		String startMonth = bulkHistDto.getStartDate();
 		String endMonth = bulkHistDto.getEndDate();
 		String svcType = bulkHistDto.getSvcType();
-		String tableName = SVC_TYPE_TABLE_MAP.getOrDefault(svcType, ConstantsUtils.SMSCLI_TBL_EVENT); // 없을 경우 기본값
+		String tableName = SVC_TYPE_TABLE_MAP.get(svcType);
 		
-		List<String> tableList = TableNameUtil.getMonthTableNames(companyCode, startMonth, endMonth, tableName, jdbcTemplateProvider.getJdbcTemplate(dbName));
+		if (tableName == null) {
+			throw new IllegalArgumentException("지원하지 않는 타입: " + svcType);
+		}
 		
+		// 월별 테이블 존재 여부 확인 및 목록 생성
+		List<String> tableList = TableNameUtil.getMonthTableNames(
+				companyCode,
+				startMonth,
+				endMonth,
+				tableName,
+				jdbcTemplateProvider.getJdbcTemplate(dbName)
+		);
+		
+		// 결과 DTO에 세팅
 		bulkHistDto.setMonthTables(tableList);
 		
 		// 수신번호 조회
