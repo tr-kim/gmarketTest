3932f5003efeed7af7692f290fb89ddab469a120
DINNOVAN\dlwog <kwogus@dinnovan.com>
2025-11-12 15:43:52 +0900

    중분류가 전체일 경우 조회 기간의 시작달만 조회 되게끔 기능 수정

--- src/main/java/com/web/gmarket/hist/service/impl/HistServiceImpl.java
+++ src/main/java/com/web/gmarket/hist/service/impl/HistServiceImpl.java
@@ -1,5 +1,8 @@
 package com.web.gmarket.hist.service.impl;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,11 +31,20 @@ public class HistServiceImpl implements HistService {
 	public List<HistDto> getHistList(HistDto histDto) {
 		Integer companyCode = histDto.getCompanyCode();
 		String dbName = DBUtils.getDBName(companyCode);
-		
-		// 월별 리스트 목록 계산
-		String startMonth = histDto.getStartDate();
-		String endMonth = histDto.getEndDate();
 		String tableName = histDto.getTableName();
+		
+		// 월 셋팅하기
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");		// yyyyMM 형식의 문자열을 파싱하기 위한 Formatter
+		YearMonth month = YearMonth.parse(histDto.getStartDate(), formatter);		
+		LocalDate startTime = month.atDay(1);										// 시작일 기준으로 시작 날짜 생성 ex) 2025-11-01
+		LocalDate endTime = month.atEndOfMonth();									// 시작일 기준으로 마지막 날짜 생성 ex) 2025-11-30
+		
+		// 중분류가 빈 값(전체 조회)일 경우 시작일만 조회
+		histDto.setStartTime(tableName.isBlank() ? startTime.toString() : histDto.getStartTime());
+		histDto.setEndTime(tableName.isBlank() ? endTime.toString() : histDto.getEndTime());
+		
+		String startDate = histDto.getStartDate();
+		String endDate = tableName.isBlank() ? histDto.getStartDate() : histDto.getEndDate();
 		
 		// 중분류 구분이 전체일 경우 전체 테이블명 가져오기
 		String tableNames = commonService.getStatCodeMapper().selectStatCodeList(companyCode, 0).stream()
@@ -42,8 +54,8 @@ public class HistServiceImpl implements HistService {
 		// 월별 테이블 존재 여부 확인 및 목록 생성
 		List<String> tableList = TableNameUtil.getMonthTableNames(
 				companyCode,
-				startMonth,
-				endMonth,
+				startDate,
+				endDate,
 				tableName.isBlank() ? tableNames : tableName,
 				jdbcTemplateProvider.getJdbcTemplate(dbName)
 		);
