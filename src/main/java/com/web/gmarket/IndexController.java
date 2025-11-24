369220e2ccc376d74768f64668cee45061489a0a
DINNOVAN\dlwog <kwogus@dinnovan.com>
2025-11-24 13:47:51 +0900

    사용자 등급이 운영자일 경우 메인 페이지가 실시간 발송 현황으로 변경

--- src/main/java/com/web/gmarket/IndexController.java
+++ src/main/java/com/web/gmarket/IndexController.java
@@ -1,8 +1,13 @@
 package com.web.gmarket;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.web.gmarket.common.auth.dto.UserDetailsDto;
+import com.web.gmarket.common.utils.UserRole;
+import com.web.gmarket.user.dto.UserDto;
 
 @Controller
 public class IndexController {
@@ -13,16 +18,21 @@ public class IndexController {
 	}
 	
 	@GetMapping("/index")
-	public String index(Model model) {
+	public String index(Authentication authentication, Model model) {
+		
+		UserDetailsDto user = (UserDetailsDto) authentication.getPrincipal();
 		
 //		model.addAttribute("layout", "/layouts/top_layout");
-		return "redirect:/view/hist";
+		// 운영자일 경우 실시간 발송 현황으로 이동
+		return user.getUserGrade() == UserRole.OPERATOR.getCode() ? "redirect:/view/real" : "redirect:/view/hist";
 	}
 	
 	@GetMapping("/index/left")
-	public String left(Model model) {
+	public String left(Authentication authentication, Model model) {
+		
+		UserDto user = (UserDto) authentication.getPrincipal();
 		
 //		model.addAttribute("layout", "/layouts/left_layout");
-		return "redirect:/view/hist"; // templates/index.html 렌더링
+		return user.getUserGrade() == UserRole.OPERATOR.getCode() ? "redirect:/view/real" : "redirect:/view/hist";
 	}
 }
