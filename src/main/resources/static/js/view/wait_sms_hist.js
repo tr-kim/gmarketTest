dcf1b8d817c679de01ebe4d44dca8667dfa33b68
DINNOVAN\dlwog <kwogus@dinnovan.com>
2025-12-01 11:10:34 +0900

    사용자 등급 및 회사 코드에 따라 기본값 설정

--- src/main/resources/static/js/view/wait_sms_hist.js
+++ src/main/resources/static/js/view/wait_sms_hist.js
@@ -27,18 +27,26 @@ $(function () {
 			minZoomLevel: "decade"
 		}
 	}).dxDateBox("instance");
+	
+	// 기본 옵션
+	const defaultOption = { code: -1, name: '선택하세요' };
+	
+	// 대분류 목록 설정
+	const companyArray = [defaultOption];
+	const companyNames = ['옥션', 'G마켓', '스마일캐시'];
+	
+	companyNames.forEach((name, idx) => {
+		if (userGrade === 0 || (userGrade === 1 && companyCode === idx)) {
+			companyArray.push({ code: idx, name });
+		}
+	});
 
 	//대분류
 	companyInstance = $('#companyCode').dxSelectBox({
-		dataSource: [
-			{ code: -1, name: '선택하세요' },
-			{ code: 0, name: '옥션' },
-			{ code: 1, name: 'G마켓' },
-			{ code: 2, name: '스마일캐시' }
-		],
+		dataSource: companyArray,
 		displayExpr: 'name',
 		valueExpr: 'code',
-		value: 1
+		value: companyCode
 	}).dxSelectBox("instance");
 
 	//제목
