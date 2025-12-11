1eed16f8b81ba6f1911c113c8040bf50b71f62d8
tr-kim <tr.kim@dinnovan.com>
2025-12-11 15:12:26 +0900

    서비스관리 코드 정리

--- src/main/resources/static/js/view/serviceMgmt.js
+++ src/main/resources/static/js/view/serviceMgmt.js
@@ -1,187 +1,190 @@
-$(function(){    
-            
-    $('#serviceListAuction').dxTreeList({
-        dataSource: auctionList,
-        keyExpr: 'svcName',
-        showSelectionControls: true,
-        selection: {
-            mode: 'multiple'
-        },
-        headerFilter: {
-            visible: false
-        },
-        showColumnHeaders: false,
-        columnChooser: {
-            enabled: false 
-        },
-        onRowClick: function(e) {
-            if (e.rowType === 'data') {
-                var component = e.component;
-                var key = e.key;
-
-                if (component.isRowSelected(key)) {
-                    component.deselectRows([key]);
-                } else {
-                    component.selectRows([key], true); 
-                }
-            }
-        },
-        toolbar: {
-            items: [
-                {
-                    location: "before",
-                    template: function() {
-                        return $("<span>")
-                            .text("옥션")
-                            .css({
-                                "font-size": "16px",
-                                "font-weight": "600",
-                                "padding-left": "5px",
-                            });
-                    }
-                },
-                {
-                    location: 'after',
-                    widget: 'dxButton',
-                    type: 'default',
-                    options: {
-                        stylingMode: 'contained',
+$(function() {
+	// 옥션
+	$('#serviceListAuction').dxTreeList({
+		dataSource: auctionList,
+		keyExpr: 'svcName',
+		selection: {
+			mode: 'multiple'
+		},
+		headerFilter: {
+			visible: false
+		},
+		showColumnHeaders: false,
+		showSelectionControls: true,
+		columnChooser: {
+			enabled: false
+		},
+		toolbar: {
+			items: [
+				{
+					location: "before",
+					template: function() {
+						return $("<span>")
+							.text("옥션")
+							.css({
+								"font-size": "16px",
+								"font-weight": "600",
+								"padding-left": "5px",
+							});
+					}
+				},
+				{
+					location: 'after',
+					widget: 'dxButton',
+					type: 'default',
+					options: {
+						stylingMode: 'contained',
 						type: 'default',
-                        text: '저장',
-                        onClick: function(e) {                            
-                            saveService('#serviceListAuction');
-                        }
-                    }
-                }                
-            ]
-        },
-        columns: [
-            { dataField: 'svcName' }
-        ]
-    });
-            
-    $('#serviceListGmarket').dxTreeList({
-        dataSource: gmarketList,
-        keyExpr: 'svcName',
-        showSelectionControls: true,
-        selection: {
-            mode: 'multiple'
-        },
-        headerFilter: {
-            visible: false
-        },
-        showColumnHeaders: false,
-        columnChooser: {
-            enabled: false 
-        },
-        onRowClick: function(e) {
-            if (e.rowType === 'data') {
-                var component = e.component;
-                var key = e.key;
-
-                if (component.isRowSelected(key)) {
-                    component.deselectRows([key]);
-                } else {
-                    component.selectRows([key], true); 
-                }
-            }
-        },
-        toolbar: {
-            items: [
-                {
-                    location: "before",
-                    template: function() {
-                        return $("<span>")
-                            .text("G마켓")
-                            .css({
-                                "font-size": "16px",
-                                "font-weight": "600",
-                                "padding-left": "5px",
-                            });
-                    }
-                },
-                {
-                    location: 'after',
-                    widget: 'dxButton',
-                    type: 'default',
-                    options: {
-                        stylingMode: 'contained',
+						text: '저장',
+						onClick: function(e) {
+							saveService('#serviceListAuction');
+						}
+					}
+				}
+			]
+		},
+		columns: [
+			{ dataField: 'svcName' }
+		],
+		onRowClick: function(e) {
+			if (e.rowType === 'data') {
+				var component = e.component;
+				var key = e.key;
+				
+				if (component.isRowSelected(key)) {
+					component.deselectRows([key]);
+				} else {
+					component.selectRows([key], true);
+				}
+			}
+		}
+	}).dxTreeList('instance');
+	
+	// G마켓
+	$('#serviceListGmarket').dxTreeList({
+		dataSource: gmarketList,
+		keyExpr: 'svcName',
+		selection: {
+			mode: 'multiple'
+		},
+		headerFilter: {
+			visible: false
+		},
+		showColumnHeaders: false,
+		showSelectionControls: true,
+		columnChooser: {
+			enabled: false
+		},
+		toolbar: {
+			items: [
+				{
+					location: "before",
+					template: function() {
+						return $("<span>")
+							.text("G마켓")
+							.css({
+								"font-size": "16px",
+								"font-weight": "600",
+								"padding-left": "5px",
+							});
+					}
+				},
+				{
+					location: 'after',
+					widget: 'dxButton',
+					type: 'default',
+					options: {
+						stylingMode: 'contained',
 						type: 'default',
-                        text: '저장',
-                        onClick() {
-                            saveService('#serviceListGmarket');
-                        }
-                    }
-                }                
-            ]
-        },
-        columns: [
-            { dataField: 'svcName' }
-        ]
-    });
-            
-    $('#serviceListSmilecash').dxTreeList({
-        dataSource: smileCashList,
-        keyExpr: 'svcName',
-        showSelectionControls: true,
-        selection: {
-            mode: 'multiple'
-        },
-        headerFilter: {
-            visible: false
-        },
-        showColumnHeaders: false,
-        columnChooser: {
-            enabled: false 
-        },
-        onRowClick: function(e) {
-            if (e.rowType === 'data') {
-                var component = e.component;
-                var key = e.key;
-
-                if (component.isRowSelected(key)) {
-                    component.deselectRows([key]);
-                } else {
-                    component.selectRows([key], true); 
-                }
-            }
-        },
-        toolbar: {
-            items: [
-                {
-                    location: "before",
-                    template: function() {
-                        return $("<span>")
-                            .text("스마일캐시")
-                            .css({
-                                "font-size": "16px",
-                                "font-weight": "600",
-                                "padding-left": "5px",
-                            });
-                    }
-                },
-                {
-                    location: 'after',
-                    widget: 'dxButton',
-                    type: 'default',
-                    options: {
-                        stylingMode: 'contained',
+						text: '저장',
+						onClick() {
+							saveService('#serviceListGmarket');
+						}
+					}
+				}
+			]
+		},
+		columns: [
+			{ dataField: 'svcName' }
+		],
+		onRowClick: function(e) {
+			if (e.rowType === 'data') {
+				var component = e.component;
+				var key = e.key;
+				
+				if (component.isRowSelected(key)) {
+					component.deselectRows([key]);
+				} else {
+					component.selectRows([key], true);
+				}
+			}
+		}
+	}).dxTreeList('instance');
+	
+	// 스마일캐시
+	$('#serviceListSmilecash').dxTreeList({
+		dataSource: smileCashList,
+		keyExpr: 'svcName',
+		selection: {
+			mode: 'multiple'
+		},
+		headerFilter: {
+			visible: false
+		},
+		showColumnHeaders: false,
+		showSelectionControls: true,
+		columnChooser: {
+			enabled: false
+		},
+		toolbar: {
+			items: [
+				{
+					location: "before",
+					template: function() {
+						return $("<span>")
+							.text("스마일캐시")
+							.css({
+								"font-size": "16px",
+								"font-weight": "600",
+								"padding-left": "5px",
+							});
+					}
+				},
+				{
+					location: 'after',
+					widget: 'dxButton',
+					type: 'default',
+					options: {
+						stylingMode: 'contained',
 						type: 'default',
-                        text: '저장',
-                        onClick() {
-                            saveService('#serviceListSmilecash');
-                        }
-                    }
-                }                
-            ]
-        },
-        columns: [
-            { dataField: 'svcName' }
-        ]
-    });
+						text: '저장',
+						onClick() {
+							saveService('#serviceListSmilecash');
+						}
+					}
+				}
+			]
+		},
+		columns: [
+			{ dataField: 'svcName' }
+		],
+		onRowClick: function(e) {
+			if (e.rowType === 'data') {
+				var component = e.component;
+				var key = e.key;
+				
+				if (component.isRowSelected(key)) {
+					component.deselectRows([key]);
+				} else {
+					component.selectRows([key], true);
+				}
+			}
+		}
+	}).dxTreeList('instance');
+	
+	function saveService(service) {
+		let treeList = $(service).dxTreeList('instance');
+		
+		console.log(treeList.getSelectedRowKeys());
+	}
 })
-function saveService(service){
-    let treeList = $(service).dxTreeList('instance');
-
-    console.log(treeList.getSelectedRowKeys());
-}
