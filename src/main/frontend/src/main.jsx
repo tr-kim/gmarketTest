e02e72443ed58fc5708fe97ca32d79ca6bf5757d
tr-kim <tr.kim@dinnovan.com>
2026-02-02 10:26:14 +0900

    한국어 패치

--- src/main/frontend/src/main.jsx
+++ src/main/frontend/src/main.jsx
@@ -1,19 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import config from 'devextreme/core/config'; 
-import { licenseKey } from './devextreme-license'; 
-
+// styles
 import 'devextreme/dist/css/dx.light.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
-
 import './scss/common.scss';
 import './scss/style.scss';
 import './fonts/fonts.css';
+
+// app
 import App from './App.jsx'
 
+// devextreme
+import config from 'devextreme/core/config';
+import { licenseKey } from './devextreme-license';
+import koMessages from './components/devextreme/ko.json';
+import { locale, loadMessages } from 'devextreme/localization';
+
 config({ licenseKey });
+loadMessages(koMessages);
+locale('ko');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
