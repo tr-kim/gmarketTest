c7963a0a4e3707b7288612f741992988547042a4
tr-kim <tr.kim@dinnovan.com>
2026-03-05 15:11:17 +0900

    store 주석 제거

--- src/main/frontend/src/pages/BulkHist/useBulkHistStore.js
+++ src/main/frontend/src/pages/BulkHist/useBulkHistStore.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 
-// Zustand는 React 바깥에 하나의 전역 store 객체 생성
 export const useBulkHistStore = create((set) => ({
   // DevExtreme CustomStore
   gridStore: null,
@@ -16,20 +15,4 @@ export const useBulkHistStore = create((set) => ({
   totalCount: 0,
   setTotalCount: (count) =>
     set({ totalCount: count }),
-
-  // // 메시지 팝업
-  // selectedMessage: '',
-  // isModalOpen: false,
-
-  // openMessage: (msg) =>
-  //   set({
-  //     selectedMessage: msg ?? '',
-  //     isModalOpen: true,
-  //   }),
-
-  // closeMessage: () =>
-  //   set({
-  //     selectedMessage: '',
-  //     isModalOpen: false,
-  //   }),
 }));
--- src/main/frontend/src/pages/Hist/useHistStore.js
+++ src/main/frontend/src/pages/Hist/useHistStore.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 
-// Zustand는 React 바깥에 하나의 전역 store 객체 생성
 export const useHistStore = create((set) => ({
   // DevExtreme CustomStore
   gridStore: null,
